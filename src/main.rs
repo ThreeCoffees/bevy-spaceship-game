ed1ad6b0daf6866ec6bfa51090f53689136c4245
ThreeCoffes <leonard.madzielewski@gmail.com>
2024-09-11 11:52:39 +0200

    Asteroids

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use asset_loader::AssetLoaderPlugin;
+use asteroids::AsteroidPlugin;
 use bevy::prelude::*;
 
 mod debug;
 mod movement;
 mod spaceship;
 mod camera;
+mod asteroids;
+mod asset_loader;
 
 use camera::CameraPlugin;
 use debug::DebugPlugin;
@@ -20,9 +24,11 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         // User configured plugins
+        .add_plugins(AssetLoaderPlugin)
         .add_plugins(SpaceshipPlugin)
         .add_plugins(MovementPlugin)
         .add_plugins(DebugPlugin)
         .add_plugins(CameraPlugin)
+        .add_plugins(AsteroidPlugin)
         .run();
 }
--- src/spaceship.rs
+++ src/spaceship.rs
@@ -1,31 +1,25 @@
 use bevy::prelude::*;
 
-use crate::movement::Velocity;
+use crate::{asset_loader::SceneAssets, movement::{Acceleration, MovingObjectBundle, Velocity}};
 
 const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
 const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
-
-#[derive(Bundle)]
-struct SpaceshipBundle {
-    velocity: Velocity,
-    model: SceneBundle,
-}
+const STARTING_ACCELERATION: Vec3 = Vec3::new(0.0, 0.0, 0.0);
 
 pub struct SpaceshipPlugin;
 
 impl Plugin for SpaceshipPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_spaceship);
+        app.add_systems(PostStartup, spawn_spaceship);
     }
 }
 
-fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(SpaceshipBundle {
-        velocity: Velocity {
-            value: STARTING_VELOCITY,
-        },
+fn spawn_spaceship(mut commands: Commands, scene_assets: Res<SceneAssets>) {
+    commands.spawn(MovingObjectBundle {
+        velocity: Velocity::new(STARTING_VELOCITY), 
+        acceleration: Acceleration::new(STARTING_ACCELERATION), 
         model: SceneBundle {
-            scene: asset_server.load("Spaceship.glb#Scene0"),
+            scene: scene_assets.spaceship.clone(),
             transform: Transform::from_translation(STARTING_TRANSLATION),
             ..default()
         },
